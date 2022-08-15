e6d6c9c3ee760377b0ec3fc13c132c3cfcba076a
Haapoja <krisu.97@hotmail.com>
2022-08-15 22:15:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// ******* VARIABLE FUNDAMENTALS *******
+
+//int x;
+//int y;
+
+//x = 7;
+//y = x + 3;
+
+//Console.WriteLine(y);
+//Console.ReadLine();
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Who are you?");
+        Console.Write("Enter first name: ");
+        string firstName;
+        firstName = Console.ReadLine();
+
+        //string lastName;
+        //Console.Write("Enter last name: ");
+        //lastName = Console.ReadLine();
+
+        Console.WriteLine("Enter last name: ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine("Hello, " + firstName + " " + lastName);
+    }
+}
+
+
